989635fb6276139278b636fb7a35b27ba16907b4
mukai <c0b21149d7@edu.teu.ac.jp>
2022-10-04 17:07:58 +0900

    追加　微調整

--- ex02/calc.py
+++ ex02/calc.py
@@ -24,7 +24,7 @@ def click_del(event):
 #練習1,(ウィンドウの作成)
 root = tk.Tk()
 root.title("不完全な電卓")
-root.configure(bg="#696969")
+root.configure(bg="#B0C4DE")
 root.geometry("290x400")
 
 #練習4,(テキスト入力欄の追加)
@@ -35,7 +35,7 @@ entry.place(x=10,y=5)
 x,y = 10,200
 numbers = list(range(9,0,-1))
 for i,num in enumerate(numbers,1):
-    btn = tk.Button(root,text=f"{num}",font=("",20),width=4,height=1,bg="#C0C0C0")
+    btn = tk.Button(root,text=f"{num}",font=("",20),width=4,height=1,bg="#5f9ea0")
     btn.bind("<1>",click_number)
     btn.place(x=x, y=y)
     x += 70
@@ -44,7 +44,7 @@ for i,num in enumerate(numbers,1):
         x = 10
 
 #0ボタンの実装
-btn = tk.Button(root,text=f"0",font=("",20),width=4,height=1,bg="#C0C0C0")
+btn = tk.Button(root,text=f"0",font=("",20),width=4,height=1,bg="#5f9ea0")
 btn.bind("<1>",click_number)
 btn.place(x=x+70,y=y)
 
@@ -56,7 +56,7 @@ btn.place(x=x+140,y=y)
 #四則演算記号の実装
 mark = ["+","-","*","/"]
 for m in mark:
-    btn = tk.Button(root,text=f"{m}",font=("",20),width=4,height=1)
+    btn = tk.Button(root,text=f"{m}",font=("",20),width=4,height=1,bg="#AFEEEE")
     btn.bind("<1>",click_number)
     y -= 50 
     btn.place(x=x+210,y=y)
@@ -69,7 +69,6 @@ btn.place(x=x+210,y=y+200)
 #クリア(削除)ボタンの実装
 btn = tk.Button(root,text=f"c",font=("",20),width=4,height=1,)
 btn.bind("<1>",click_del)
-btn.place(x=x+140,y=y)
-print(x,y)
+btn.place(x=x+210,y=y-50)
 
 root.mainloop()
