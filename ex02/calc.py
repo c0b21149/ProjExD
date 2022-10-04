d59b33d1d36bffcbc1bddc9fc31cb1daff5f7db4
mukai <c0b21149d7@edu.teu.ac.jp>
2022-10-04 14:56:43 +0900

    練習６まで

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,11 +6,12 @@ from tkinter.tix import COLUMN
 #練習3
 def click_number(event):
     btn = event.widget
-    num = int(btn["text"])
+    num = btn["text"]
     #tkm.showinfo(f"{num}",f"{num}のボタンが押されました")
 
     #練習5
-    entry.insert(0,num)
+    entry.insert(tk.END,num)
+
 
 #練習1
 root = tk.Tk()
@@ -20,9 +21,11 @@ root.geometry("300x500")
 entry=tk.Entry(root,width=10, font=("",40),justify="right")
 entry.grid(row=0, column=0, columnspan=3)
 
-#練習2
+#練習2,練習６
 r,c = 1,0
-for i,num in enumerate(range(9,-1,-1),1):
+numbers = list(range(9,-1,-1))
+operators = ["+"]
+for i,num in enumerate(numbers+operators,1):
     btn = tk.Button(root,text=f"{num}",font=("",30),width=4,height=2)
     btn.bind("<1>",click_number)
     btn.grid(row=r,column=c)
