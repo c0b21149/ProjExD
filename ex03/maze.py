439a4f491547cd46a5892aafaf8d2e7dc5107c65
mukai <c0b21149d7@edu.teu.ac.jp>
2022-10-18 14:26:04 +0900

    練習4

--- ex03/maze.py
+++ ex03/maze.py
@@ -6,12 +6,16 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")#練習１
 
+    #練習２
     canv = tk.Canvas(root,width=1500,height=900,bg="black")
     canv.pack()
 
+    #練習３
     tori = tk.PhotoImage(file = "ex03/fig/5.png")
     cx, cy = 300, 400
     canv.create_image(cx,cy,image=tori,tag="tori")
 
+    #練習４
+    key = ""#現在押されているキーを表す
 
     root.mainloop()
