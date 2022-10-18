32011eab4ccdef5f7877b48ca50be64458dd4684
mukai <c0b21149d7@edu.teu.ac.jp>
2022-10-18 16:19:13 +0900

    画像変更追加

--- ex03/maze.py
+++ ex03/maze.py
@@ -12,16 +12,37 @@ def key_up(event):
 #練習７
 def main_proc():
     global cx,cy
-    if key == "Up":
-        cy -= 20
-    if key == "Down":
-        cy += 20
-    if key == "Left":
-        cx -= 20
-    if key == "Right":
-        cx += 20
+    global mx, my
+    global tori
+    delta = {
+        ""     :[0,0],
+        "Up"   :[0,-1],
+        "Down" :[0,+1],
+        "Left" :[-1,0],
+        "Right":[+1,0],
+    }
+
+    if maze_lst[my+delta[key][1]][mx+delta[key][0]] == 0:
+        mx, my = mx+delta[key][0], my+delta[key][1]
+        cx, cy = mx*100+50, my*100+50
+        if key == "Up":
+            tori = tk.PhotoImage(file = "ex03/fig/3.png")
+            canv.create_image(cx,cy,image=tori,tag="tori")
+        if key == "Down":
+            tori = tk.PhotoImage(file = "ex03/fig/3.png")
+            canv.create_image(cx,cy,image=tori,tag="tori")
+        if key == "Right":
+            tori = tk.PhotoImage(file = "ex03/fig/2.png")
+            canv.create_image(cx,cy,image=tori,tag="tori")
+        if key == "Left":
+            tori = tk.PhotoImage(file = "ex03/fig/5.png")
+            canv.create_image(cx,cy,image=tori,tag="tori")
+    else: #壁に向かって進もうとする場合
+        tori = tk.PhotoImage(file = "ex03/fig/0.png")
+        canv.create_image(cx,cy,image=tori,tag="tori")
     canv.coords("tori",cx, cy)
     root.after(100, main_proc)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -38,7 +59,8 @@ if __name__ == "__main__":
 
     #練習３
     tori = tk.PhotoImage(file = "ex03/fig/5.png")
-    cx, cy = 300, 400
+    mx, my = 1, 1
+    cx, cy = mx*100+50, my*100+50
     canv.create_image(cx,cy,image=tori,tag="tori")
 
     #練習４
