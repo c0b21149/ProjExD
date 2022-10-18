159e40e8b80046cb71f35cdd6ce97bb29a80f9dc
mukai <c0b21149d7@edu.teu.ac.jp>
2022-10-18 14:19:52 +0900

    練習３

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,8 +1,17 @@
+from email.mime import image
 import tkinter as tk
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("迷えるこうかとん")
+    root.title("迷えるこうかとん")#練習１
+
+    canv = tk.Canvas(root,width=1500,height=900,bg="black")
+    canv.pack()
+
+    tori = tk.PhotoImage(file = "ex03/fig/5.png")
+    cx, cy = 300, 400
+    canv.create_image(cx,cy,image=tori,tag="tori")
+
 
     root.mainloop()
